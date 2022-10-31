9da4fdae737443228fefeff1b6d64e456d1e7a55
Porthos <etdds@protonmail.com>
2022-10-31 10:12:25 +0000

    Add spacy to requirements and package.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     setuptools.setup(
         name="redditquotebot",
         author="Porthos",
-        author_email="stuian@protonmail.com",
+        author_email="etdds@protonmail.com",
         description="Reddit bot for detecting and replying to famous quotes",
         long_description=long_description,
         long_description_content_type="text/markdown",
@@ -19,6 +19,10 @@ with open("README.md", "r") as fh:
             "Operating System :: OS Independent"
         ],
         python_requires='>=3.6',
-        install_requires=["praw==7.6.0"],
+        install_requires=[
+            "praw==7.6.0", 
+            "spacy==3.4.2", 
+            "en-core-web-lg @ https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.4.1/en_core_web_lg-3.4.1-py3-none-any.whl"
+            ],
         extras_require={},
     )
